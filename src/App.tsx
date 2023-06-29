103c97a8138b7c4ccc72f50dd76263f9ba5e5888
Folajimi <folajimiopeyemisax13@gmail.com>
2023-06-29 18:43:07 +0100

    creating helpers functions

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,10 @@
 import { Router } from "./Routes";
 import React from "react";
 import GlobalStyle from "./Shared/Globalstyles";
-
+import { create_UUID,numberWithCommas } from "./Shared/Utils/helperFunctions";
+const orderId = create_UUID()
+console.log(orderId)
+console.log(numberWithCommas(1000000000000))
 const app : React.FC = ()=> {
     return (
       <>
---
+++ src/Shared/Utils/helperFunctions.ts
@@ -0,0 +1,142 @@
+export function debounce(func: Function, delay: number) {
+  let timeoutId: number;
+
+  return (...args: any[]) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      func.apply(null, args);
+    }, delay);
+  };
+}
+
+export const getFormattedDate = (date: Date): string => {
+  return date.toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+  });
+};
+
+export function isEmpty(value: any): boolean {
+  if (Array.isArray(value)) {
+    return value.length === 0;
+  }
+
+  if (typeof value === "object" && value !== null) {
+    return Object.keys(value).length === 0;
+  }
+
+  return !value;
+}
+
+export const validateEmail = (email: string): boolean => {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+};
+
+export function deepClone<T>(obj: T): T {
+  return JSON.parse(JSON.stringify(obj));
+}
+
+export const formatCurrency = (
+  value: number,
+  currency: string,
+  decimalPlaces: number = 2
+): string => {
+  return `${currency}${value.toFixed(decimalPlaces)}`;
+};
+
+export const calculateDiscount = (
+  originalPrice: number,
+  discountPercentage: number
+): number => {
+  const discountAmount = (originalPrice * discountPercentage) / 100;
+  return originalPrice - discountAmount;
+};
+
+export const generateSlug = (text: string): string => {
+  return text
+    .toLowerCase()
+    .replace(/\s+/g, "-")
+    .replace(/[^\w-]+/g, "");
+};
+
+//To be refactored
+export const calculateTotalPrice = (items: any[]): number => {
+  return items.reduce((total, item) => total + item.price * item.quantity, 0);
+};
+
+//To be refactored
+export const sortProductsByPrice = (
+  products: any[],
+  ascending: boolean = true
+): any[] => {
+  return products.sort((a, b) =>
+    ascending ? a.price - b.price : b.price - a.price
+  );
+};
+
+export const filterProductsByCategory = (
+  products: any[],
+  category: string
+): any[] => {
+  return products.filter((product) => product.category === category);
+};
+
+//to be refactored
+export const getAverageRating = (reviews: any[]): number => {
+  if (reviews.length === 0) return 0;
+  const totalRating = reviews.reduce(
+    (total, review) => total + review.rating,
+    0
+  );
+  return totalRating / reviews.length;
+};
+
+//to  be refactored
+export const formatAddress = (address: any): string => {
+  return `${address.street}\n${address.city}, ${address.state} ${address.zipCode}`;
+};
+
+export const calculateShippingCost = (
+  totalWeight: number,
+  shippingRate: number
+): number => {
+  return totalWeight * shippingRate;
+};
+
+// export const generateOrderNumber = (): string => {
+//   const timestamp = Date.now().toString();
+//   const randomDigits = Math.floor(Math.random() * 1000)
+//     .toString()
+//     .padStart(3, "0");
+//   return `Am${timestamp}${randomDigits}`;
+// };
+
+export const truncateText = (text: string, maxLength: number): string => {
+  if (text.length <= maxLength) return text;
+  return text.slice(0, maxLength - 3) + "...";
+};
+
+export function numberWithCommas(x: number | string): string {
+  if (x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
+  return ""
+}
+
+export function formatMessageTimestamp(timestamp: number): string {
+  if (!timestamp) return "";
+  const date = new Date(timestamp);
+  return date.toLocaleTimeString().slice(0, 5);
+}
+
+export function create_UUID(): string {
+  let dt = new Date().getTime();
+  const uuid = "am-x2xx-xxxx-4xxx-yxxx".replace(/[xy]/g, (c) => {
+    const r = (dt + Math.random() * 16) % 16 | 0;
+    dt = Math.floor(dt / 16);
+    return (c === "x" ? r : (r & 0x3) | 0x8).toString(16);
+  });
+  return uuid;
+}
