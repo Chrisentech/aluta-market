f360049cd2339d7f590bb59b26e12ceb0e1a3465
unknown <tayeakinrele@gmail.com>
2023-08-12 16:05:51 +0100

    navigation on the product view page

--- src/Pages/Private/Buyer/ProductView/ProductView.tsx
+++ src/Pages/Private/Buyer/ProductView/ProductView.tsx
@@ -1,11 +1,81 @@
 import React from "react";
-import { Wrapper } from "./productview.styles";
-// import {Layout} from "../../../Layout"
+import { Wrapper,Section, NavBar, NavBar2, GridItem, Main,  } from "./productview.styles";
+import Layout from "../../../../Layouts";
+import { View, ImageCard, Navbar } from "../../../../Shared/Components";
+import { cloth } from "../../../../assets/index.tsx";
+// Icons
+import { IoIosCheckmark} from "react-icons/io";
+import {HiOutlineChevronRight} from "react-icons/hi";
+import {HiOutlineBars3} from "react-icons/hi2";
+import {RxCaretDown} from "react-icons/rx";
+
+
+const gridItem = [
+  <GridItem>
+    <div>
+      <ImageCard src={cloth} alt="#" width="100" height="100"></ImageCard> 
+      <div className="check">
+        <IoIosCheckmark color="#00b517" size="45" />
+        <span>In stock</span>
+      </div>
+    </div>
+  </GridItem>
+];
 
 const Screen: React.FC = () => {
   return (
     <Wrapper>
-      <div className="">Hello Product View</div>
+      <NavBar>
+        <div className="navbar">
+          <ul>
+            <li><a href='#'> <HiOutlineBars3 /> All category</a></li>
+            <li><a href='#'>Hot offers</a></li>
+            <li><a href='#'>Skynet</a></li>
+            <li><a href='#'>Aluta Food</a></li>
+            <li><a href='#'>Restaurants</a></li>
+            <li><a href='#'>Help <RxCaretDown/></a></li>
+          </ul>
+        </div>
+      </NavBar>
+      <Section>
+        <NavBar2>
+          <div className="navbar2">
+              <ul>
+                <li><a href='#'>Home</a></li>
+                <li><HiOutlineChevronRight/></li>
+                <li><a href='#'>Clothing</a></li>
+                <li><HiOutlineChevronRight/></li>
+                <li><a href='#'>Men's wear</a></li>
+                <li><HiOutlineChevronRight/></li>
+                <li><a href='#'>Summer clothing</a></li>
+              </ul>
+            </div>
+        </NavBar2>
+        <View mode="grid" gridItems= {gridItem}/>
+        <Main>
+          <div className="productDesc">
+            <span>Description</span>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Commodi voluptates consequuntur amet quam, et, placeat 
+              sapiente dolorum tempore, quas esse eveniet! Odit enim 
+              accusamus eveniet suscipit iste voluptatem necessitatibus? 
+              Illum. Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Commodi voluptates consequuntur amet quam, et, placeat 
+              sapiente dolorum tempore, quas esse eveniet! Odit enim 
+              accusamus eveniet suscipit iste voluptatem necessitatibus? 
+              Illum.Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Commodi voluptates consequuntur amet quam, et, placeat 
+              sapiente dolorum tempore, quas esse eveniet! Odit enim 
+              accusamus eveniet suscipit iste voluptatem necessitatibus? 
+              Illum.Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Commodi voluptates consequuntur amet quam, et, placeat 
+              sapiente dolorum tempore, quas esse eveniet! Odit enim 
+              accusamus eveniet suscipit iste voluptatem necessitatibus? 
+              Illum.</p>
+          </div>
+        </Main>
+      </Section>
+      
     </Wrapper>
   );
 };
@@ -13,5 +83,8 @@ const Screen: React.FC = () => {
 // const ProductView: React.FC = () => {
 //     <Layout component={Screen} />
 // };
+const ProductView = () => {
+  return <Layout layout={"full"} component={Screen} state={false} />;
+};
 
-export default Screen;
+export default ProductView;
--- src/Pages/Private/Buyer/ProductView/productview.styles.ts
+++ src/Pages/Private/Buyer/ProductView/productview.styles.ts
@@ -1,5 +1,129 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-  margin-left: 20px;
-`;
+  background-color: #F7FAFC;
+  overflow-x: hidden;
+  text-align: justify;
+  h2 {
+    color: var(--dark, #1c1c1c);
+    font-feature-settings: "clig" off, "liga" off;
+    /* Title-H3 */
+    font-family: Inter;
+    font-size: 24px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: 32px; /* 133.333% */
+    letter-spacing: -0.2px;
+  }
+`
+export const NavBar = styled.div`
+  // display: flex;
+  gap: 20px;
+  background-color: #fff;
+  
+  .navbar ul{
+    list-style-type: none;
+    align-items: center;
+    overflow: hidden;
+    border-width: thin 0px thin 0px;
+    border-style: solid;
+    border-color: #737373;
+    margin: 15px auto;
+    padding: 20px;
+    
+
+    li {
+      font-style: normal;
+      font-size: 12px;
+      font-weight: 500;
+      display: inline;
+    }
+
+    li a{
+      padding: 10px;
+    }
+
+    li a:hover{
+      color: #FF4C16;
+    }
+  }
+`
+export const Section = styled.div`
+  margin: auto;
+  width: 80%;
+  padding: 0px;
+`
+export const NavBar2 = styled.div`
+  display: flex;
+  align-items: center;
+  color: #4d4d4d;
+
+  .navbar2 ul{
+    list-style-type: none;
+    // align-items: center;
+    // justify-content: space-between;
+    overflow: hidden;
+    margin: 0;
+    padding: 0;
+    
+    li {
+      font-style: normal;
+      font-size: 12px;
+      font-weight: 500;
+      display: inline;
+    }
+
+    li a{
+      padding: 0px 5px;
+    }
+
+    li a:hover{
+      color: #FF4C16;
+    }
+`
+export const GridItem = styled.div<{ background?: string }>`
+  display: flex;
+  gap: 20px;
+  align-items: center;
+  height: 100%;
+  position: relative;
+
+//   .check {
+//     display: flex;
+//     justify-content: space-between;
+//     display: flex;
+//     margin: 30px 0;
+//     align-items: center;
+//     color: var(--green, #00b517);
+//     font-family: Inter;
+//     font-size: 14px;
+//     font-style: normal;
+//     // font-weight: 700;
+//     line-height: 16px; /* 114.286% */
+//     letter-spacing: -0.28px;
+ 
+//   }
+// `;
+
+export const Main = styled.div`
+  .productDesc{
+    align-items: center;
+    height: 100%;
+    justify-content: center;
+    color: var(--dark, #1c1c1c);
+    font-feature-settings: "clig" off, "liga" off;
+
+    span{
+      font-family: Inter;
+      font-size: 20px;
+      font-style: normal;
+      font-weight: 600;
+      line-height: 32px; /* 133.333% */
+      letter-spacing: -0.2px;
+    }
+
+  }
+
+`
+
+
