61932debd38052619c7d639817c9e9994e7ed776
unknown <tayeakinrele@gmail.com>
2023-08-19 15:59:07 +0100

    updated product view page

--- src/Pages/Private/Buyer/ProductView/ProductView.tsx
+++ src/Pages/Private/Buyer/ProductView/ProductView.tsx
@@ -1,22 +1,73 @@
 import React from "react";
-import { Wrapper,Section, NavBar, NavBar2, GridItem, Main,  } from "./productview.styles";
+import { 
+  Wrapper,
+  Section, 
+  NavBar, 
+  NavBar2, 
+  GridItem, 
+  Main,  
+} from "./productview.styles";
 import Layout from "../../../../Layouts";
-import { View, ImageCard, Navbar } from "../../../../Shared/Components";
+import { View, ImageCard, Rating } from "../../../../Shared/Components";
 import { cloth } from "../../../../assets/index.tsx";
 // Icons
 import { IoIosCheckmark} from "react-icons/io";
-import {HiOutlineChevronRight} from "react-icons/hi";
+import {HiOutlineChevronRight, HiOutlineHeart} from "react-icons/hi";
 import {HiOutlineBars3} from "react-icons/hi2";
 import {RxCaretDown} from "react-icons/rx";
+import {BiCommentDetail} from "react-icons/bi";
+import {AiTwotoneMinusCircle} from "react-icons/ai";
+import {MdOutlineShoppingBasket} from "react-icons/md";
+import {TbH3, TbLetterA} from "react-icons/tb"
 
 
 const gridItem = [
   <GridItem>
-    <div>
-      <ImageCard src={cloth} alt="#" width="100" height="100"></ImageCard> 
-      <div className="check">
-        <IoIosCheckmark color="#00b517" size="45" />
+    <div className="productImage">
+      <ImageCard src={cloth} view="grid" width="50"></ImageCard> 
+    </div>
+    <div className="productCheck">
+      <div style={{color:"#00b517", fontSize:15}}>
+        <IoIosCheckmark size="20"/>
         <span>In stock</span>
+      </div>
+      <div>
+        <span className="desc">Men's Short Sleeve T-Shirt Cotton Base Layer Slim Muscle</span>
+        <span><HiOutlineHeart style={{color: "#fa3434", float: "right"}} size={30}/></span>
+      </div>
+      <div style={{color:"#999999", }}>
+        <span><Rating numberOfRates={5}/></span>
+        <span> 4.3</span>
+        <span><AiTwotoneMinusCircle size={8} style={{margin: "2px 10px"}}/></span>
+        <span><BiCommentDetail/> 32 reviews</span>
+        <span><AiTwotoneMinusCircle size={8} style={{margin: "2px 10px"}}/></span>
+        <span><MdOutlineShoppingBasket/> 154 sold</span>
+      </div>
+      <div className="productPrice">
+        <h1>N4,500.00</h1>
+        <p><s>N4800</s><span style={{color: "#fa3434"}}> -32%</span></p>
+      </div>
+      <div className="colors">
+        <h4>Colors:</h4>
+      </div>
+      <div className="size">
+        <h4>Size:</h4>
+      </div>
+      <div style={{margin: "10px 0"}}>
+        <button id="button1" type="submit">Buy now</button>
+        <button id="button2" type="submit">Add to cart</button>
+      </div>
+
+    </div>
+    <div className="productInfo">
+      <div>
+        <div className="sellerIcon">
+          <TbLetterA color={"#7dc5c4"} size={30}/> 
+        </div>
+        <div className="sellerDesc">
+          <h3>Seller's Information</h3>
+          <p>Arike Collection</p>
+        </div>
       </div>
     </div>
   </GridItem>
@@ -51,7 +102,7 @@ const Screen: React.FC = () => {
               </ul>
             </div>
         </NavBar2>
-        <View mode="grid" gridItems= {gridItem}/>
+        <View mode="grid" gridItems= {gridItem} itempergrid={1}/>
         <Main>
           <div className="productDesc">
             <span>Description</span>
--- src/Pages/Private/Buyer/ProductView/productview.styles.ts
+++ src/Pages/Private/Buyer/ProductView/productview.styles.ts
@@ -8,14 +8,40 @@ export const Wrapper = styled.div`
     color: var(--dark, #1c1c1c);
     font-feature-settings: "clig" off, "liga" off;
     /* Title-H3 */
-    font-family: Inter;
-    font-size: 24px;
-    font-style: normal;
-    font-weight: 600;
-    line-height: 32px; /* 133.333% */
-    letter-spacing: -0.2px;
+    
   }
 `
+export const IconWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  align-items: center;
+  color: #bdc4cd;
+  margin: 0 10px;
+  cursor: pointer;
+  svg {
+    cursor: pointer;
+    transition: 0.3s ease-in-out;
+    font-size: 20px;
+  }
+  label {
+    transition: 0.3s ease-in-out;
+    cursor: pointer;
+    text-align: center;
+    font-feature-settings: "clig" off, "liga" off;
+    font-family: Inter;
+    font-size: 12px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+  }
+  &:hover {
+    svg,
+    label {
+      color: #ff001f !important;
+    }
+  }
+`;
 export const NavBar = styled.div`
   // display: flex;
   gap: 20px;
@@ -51,7 +77,8 @@ export const NavBar = styled.div`
 export const Section = styled.div`
   margin: auto;
   width: 80%;
-  padding: 0px;
+  padding: 10px;
+  // justify-content: center;
 `
 export const NavBar2 = styled.div`
   display: flex;
@@ -87,6 +114,107 @@ export const GridItem = styled.div<{ background?: string }>`
   align-items: center;
   height: 100%;
   position: relative;
+  background-color: #fff;
+
+  .productImage{
+    // float: left;
+    width: 35%;
+    border-style: solid;
+    border-width: 1px;
+    border-color: #999999;
+    border-radius: 5px;
+    padding: 15px;    
+  }
+
+  .productCheck{
+    margin: 20px;
+    // float: left;
+    width: 40%;
+
+    div .desc{
+      font-family: Inter;
+      font-size: 20px;
+      font-style: normal;
+      font-weight: 600;
+      line-height: 32px; /* 133.333% */
+      letter-spacing: -0.2px;
+    }
+
+    .productPrice{
+      background-color: #fff0df;
+      padding: 10px 15px;
+      margin: 15px 0;
+      
+      h1{
+        font-size: 40px;
+        color: #fa3434;
+      }
+      p{
+        font-size: 20px;
+        color: #999999;
+      }
+    }
+    button {
+      flex: 1;
+      padding: 3% 15%;
+      border-radius: 6px;
+      cursor: pointer;
+      font-size:15px;
+    }
+    #button1{
+      background: var(--pr, linear-gradient(180deg, #ff7612 0%, #ff001f 100%));
+      color: #fff;
+      outline: 0;
+      border: 0;
+    }
+
+    #button2{
+      background: #fff;
+      color: var(--pr, linear-gradient(180deg, #ff7612 0%, #ff001f 100%));
+      outline: 1;
+      border-style: solid;
+      border-width: 1px;
+      border-color: #999999;
+      float: right;
+    }
+  }
+
+  .productInfo{
+    // float: right;
+    width: 25%;
+    border-style: solid;
+    border-width: 1px;
+    border-color: #999999;
+    border-radius: 5px;
+    padding: 10px;  
+    align-items:center;
+
+    div .sellerIcon{
+      float:left;
+      vertical-align: middle;
+      line-height: 20px;
+      background-color: #c6f3f1;
+      padding: 10px;
+      margin: 0 10px;
+    }
+
+    div .sellerDesc{
+      padding: 10px 0;
+
+      h3{
+        font-family: Inter;
+        font-size: 20px;
+        font-style: normal;
+        font-weight: 600;
+        line-height: 32px; /* 133.333% */
+        letter-spacing: -0.2px;
+      }
+    }
+  }
+
+
+
+
 
 //   .check {
 //     display: flex;
@@ -103,7 +231,7 @@ export const GridItem = styled.div<{ background?: string }>`
 //     letter-spacing: -0.28px;
  
 //   }
-// `;
+// `
 
 export const Main = styled.div`
   .productDesc{
