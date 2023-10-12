2791ce654f50738ff0c19be14396ad850f5e935d
Folexy13 <folajimiopeyemisax13@gmail.com>
2023-10-12 15:02:00 +0100

    [feat]: Quick Update to the Apollo client

--- src/Services/graphql/apolloClient.ts
+++ src/Services/graphql/apolloClient.ts
@@ -5,7 +5,7 @@ const httpLink = new HttpLink({
   // uri: "http://localhost:8082/graphql", //Developement
   uri: "http://54.161.147.89:8082/graphql", //Production
   headers: {
-    Authorization: `Bearer ${token}`,
+    Authorization:token ? `Bearer ${token}`: " ",
   },
 });
 
