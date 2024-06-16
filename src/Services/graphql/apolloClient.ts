6a5b69ca78bbbc20e1910b6a451c8acd44118fd8
Foley13 <folajimiopeyemisax13@gmail.com>
2024-06-16 15:15:55 +0100

    [FEAT]:Quick Ui Chnages...

--- src/Services/graphql/apolloClient.ts
+++ src/Services/graphql/apolloClient.ts
@@ -3,7 +3,7 @@ import { getCookie } from "../../Shared/Utils/helperFunctions";
 const token = getCookie("access_token") ?? null;
 
 const httpLink = new HttpLink({
-	uri: "https://aluta-market-server-a841f6d3f7b8.herokuapp.com/graphql", //Developement
+	uri: "https://api.thealutamarket.com/graphql", //Developement
 	// uri: "https://aluta-market-api.onrender.com/graphql", //Production
 	// uri: "http://localhost:8082/graphql", //Production
 	headers: {
