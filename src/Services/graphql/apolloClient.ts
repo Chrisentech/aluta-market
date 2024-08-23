5cd201455a2079e0f2a36f0bcbd321213197490a
Aluko Opeyemi Folajimi <62119915+Folexy13@users.noreply.github.com>
2024-08-23 11:52:10 -0700

    Update apolloClient.ts

--- src/Services/graphql/apolloClient.ts
+++ src/Services/graphql/apolloClient.ts
@@ -3,8 +3,8 @@ import { getCookie } from "../../Shared/Utils/helperFunctions";
 const token = getCookie("access_token") ?? null;
 
 const httpLink = new HttpLink({
-	uri: "https://api.thealutamarket.com/graphql", //Production
-	// uri: "https://aluta-market-api.onrender.com/graphql", //Production
+	//uri: "https://api.thealutamarket.com/graphql", //Production
+	uri: "https://aluta-market-api.onrender.com/graphql", //Production
 	// uri: "http://localhost:8082/graphql", //Development
 	headers: {
 		Authorization: token ? `Bearer ${token}` : "",
